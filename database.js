0c24d99c691fd572337772708d0609e364a39121
isaacdll23 <isaacdelalama1013@gmail.com>
2022-11-21 18:51:02 -0600

    initial commit

--- database.js
+++
@@ -1,7 +0,0 @@
-const pgp = require('pg-promise')()
-const { connectionString } = require("./config.json")
-const dotenv = require('dotenv').config()
-
-const db = pgp(process.env.connectionString)
-
-module.exports = db
